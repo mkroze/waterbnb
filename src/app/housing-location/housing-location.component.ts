1e60e0bb70b6723f0fa70417fae4a093df31f518
mkroze <mkblue1976@gmail.com>
2024-03-22 04:50:01 +0000

    youtube tutorial about angular

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housing-location";
+import { RouterModule } from "@angular/router";
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-locationn">{{ housingLocation.city }},{{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn more</a>
+    </section>
+  `,
+  styleUrls: ["./housing-location.component.css"],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
---
+++ src/app/housing.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HousingLocation } from "./housing-location";
+@Injectable({
+  providedIn: "root",
+})
+export class HousingService {
+  url = "http://localhost:3000/location"
+  constructor() {}
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url)
+    return (await data).json() ?? [];
+  }
+  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined >{
+    const data = await fetch(`${this.url}/${id}`)
+    return (await data).json() ?? {};
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+  }
+}
